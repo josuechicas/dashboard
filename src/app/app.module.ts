affa47b142831c344caaabd234768ad38d4be7c7
Josue <josuechicas>
2020-01-28 09:44:51 -0600

    dashboard

---
+++ src/app/app.module.ts
@@ -0,0 +1,70 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+//import { UserComponent } from './user/user.component';
+//import { HomeComponent } from './home/home.component';
+//import { AdminComponent } from './admin/admin.component';
+//import { PmComponent } from './pm/pm.component';
+
+//import { httpInterceptorProviders } from './auth/auth-interceptor';
+
+
+
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './main/header/header.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { MenuSidebarComponent } from './main/menu-sidebar/menu-sidebar.component';
+import { BlankComponent } from './pages/blank/blank.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ToastrModule } from 'ngx-toastr';
+import { MessagesDropdownMenuComponent } from './main/header/messages-dropdown-menu/messages-dropdown-menu.component';
+import { NotificationsDropdownMenuComponent } from './main/header/notifications-dropdown-menu/notifications-dropdown-menu.component';
+
+@NgModule({
+  declarations: [
+    //UserComponent,
+    //HomeComponent,
+    //AdminComponent,
+    //PmComponent,
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuSidebarComponent,
+    BlankComponent,
+    ProfileComponent,
+    RegisterComponent,
+    DashboardComponent,
+    MessagesDropdownMenuComponent,
+    NotificationsDropdownMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
